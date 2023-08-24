0b94bbeeeab28b48e77cd2162a8278fa842ec231
nguyennhatanh2103@gmail.com <anhnn.bi12-023@st.usth.edu.vn>
2023-08-24 10:56:06 +0700

    Labwork 2

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -0,0 +1,47 @@
+package vn.edu.usth.usthweather;
+
+import android.os.Bundle;
+import android.util.Log;
+//import android.view.View;
+//import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeatherActivity extends AppCompatActivity {
+    private static final String TAG = "WeatherActivity";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.i(TAG,"onCreate: This is a log message.");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: This is a log message.");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: This is a log message.");
+    }
+    @Override
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: This is a log message.");
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: This is a log message.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStart: This is a log message.");
+    }
+
+}
